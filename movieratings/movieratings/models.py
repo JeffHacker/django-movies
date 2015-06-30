41fef57ebe51688672c1bc9e661ad8677fc9d26b
Jeff Hacker <reachjkh@yahoo.com>
2015-06-30 13:42:46 -0400

    commit before fresh start

--- movieratings/movieratings/models.py
+++ movieratings/movieratings/models.py
@@ -9,3 +9,12 @@ class Food(models.Model):
 class Person(models.Model):
     name = models.CharField(max_length=100)
     favorite_food = models.ForeignKey(Food)
+
+
+from your_app.models import Food, Person
+
+my_favorite_food = Food.objects.create(name="Hamburgers", spicy=True)
+joel = Person.objects.create(name="Joel", favorite_food=my_favorite_food)
+
+# Is Joel's favorite food spicy?
+print(joel.favorite_food.spicy)
