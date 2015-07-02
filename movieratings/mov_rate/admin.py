c92328fc297754ef519b1aa9110c3f97136ed68f
Jeff Hacker <reachjkh@yahoo.com>
2015-07-01 20:09:57 -0400

    movies on webpages

--- movieratings/mov_rate/admin.py
+++ movieratings/mov_rate/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from mov_rate.models import Movie
+
+admin.site.register(Movie)
--- movieratings/mov_rate/migrations/0004_auto_20150701_2241.py
+++ movieratings/mov_rate/migrations/0004_auto_20150701_2241.py
@@ -13,13 +13,13 @@ def create_movie_data(x, y):
         for movie in comma_fix[1:]:
             movie_list = movie #.replace("\n", "").split(',')
             genre_list = movie_list.pop().split('|')
-            d = {_.lower(): True for _ in genre_list}
-            #Movie.objects.create(id=movie_list[0], title=movie_list[1], **d)
+            d = {_.lower().replace("-", "_"): True for _ in genre_list}
+            Movie.objects.create(id=movie_list[0], title=movie_list[1], **d)
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mov_rate', '0001_initial'),
+        ('mov_rate', '0003_movie_imax'),
     ]
 
     operations = [
