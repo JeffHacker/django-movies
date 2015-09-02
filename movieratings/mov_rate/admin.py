9d54f84cc778ee7c7720dc45e8bf8bd331a80d99
Jeff Hacker <reachjkh@yahoo.com>
2015-09-01 22:02:35 -0400

    removed

--- movieratings/mov_rate/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from mov_rate.models import Movie
-from mov_rate.models import Rating
-
-admin.site.register(Movie)
-admin.site.register(Rating)
